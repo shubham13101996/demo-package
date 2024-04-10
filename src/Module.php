1d9df61ca77d96c1ac92f50d54039f11779f735f
DESKTOP-4NR8DP8\Admin <shubham.pathak926@gmail.com>
2024-04-10 15:03:45 +0530

    package created

---
+++ src/Module.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace demosamarth;
+
+/**
+ * demo module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'demosamarth\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+    }
+}
